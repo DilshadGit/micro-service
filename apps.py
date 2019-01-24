dca7fbfeb20de673c5fa606c8d9977220f452515
DilshadGit <dilshad.abdulla@icloud.com>
2019-01-24 10:33:46 +0000

    Change apps to app and add yaml for test

--- apps.py
+++
@@ -1,9 +0,0 @@
-import connexion
-
-from connexion.resolver import RestyResolver
-
-
-if __name__ == '__main__':
-	apps = connexion.App(__name__, 9090, specification_dir='swagger/')
-	apps.add_api('my_super_app.yaml', resolver=RestyResolver('api'))
-	apps.run()
